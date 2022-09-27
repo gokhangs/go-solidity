a173e8626c48e657c4f0c5c21e7a07a441f77f19
Gokhan Sagirlar <gsagirlar@qpq.io>
2022-09-27 22:54:42 +0300

    contract, abi and bin

---
+++ contract/todo.sol
@@ -0,0 +1,56 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Todo{
+    Task[] tasks; //mapping
+    address public owner;
+
+    struct Task{
+        string content;
+        bool status;
+    }
+
+    modifier isOwner(){
+        require(owner==msg.sender);
+        _;
+    }
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    //memory means we pass "content" as copy
+    function add(string memory _content) public isOwner{
+        tasks.push(Task(_content, false));
+    }
+
+    function get(uint _id) public view returns (Task memory){
+        return tasks[_id];
+    }
+
+    function list() public view returns (Task[] memory) {
+        return tasks;
+    }
+
+    function toggle(uint _id) public isOwner {
+        tasks[_id].status = !tasks[_id].status;
+    }
+
+    function update(uint _id, string memory _content, bool status) public isOwner{ //} public returns (Task memory){
+        tasks[_id].status = status;
+        tasks[_id].content = _content;
+        //return tasks[_id];
+    }
+
+    function remove(uint _id) public isOwner{
+
+        //delete keyword sets value to default value
+        // delete tasks[_id];
+        for(uint i = _id; i < tasks.length -1; i++){
+            tasks[_id] = tasks[_id +1];
+        }
+        tasks.pop();
+    }
+
+
+}
