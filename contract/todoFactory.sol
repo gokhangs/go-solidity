ea9e840f9945a627177772c07b2d0691b43c72cc
Gokhan Sagirlar <gsagirlar@qpq.io>
2022-09-28 16:34:43 +0300

    add todo factory

---
+++ contract/todoFactory.sol
@@ -0,0 +1,25 @@
+// SPDX-License_Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./todo.sol";
+
+contract TodoFactory {
+
+Todo[] public todoArray;
+
+function createTodoContract() public {
+Todo todo = new Todo();
+todoArray.push(todo);
+}
+
+function tFAdd(uint256 _todoIndex, string memory content) public {
+return Todo(address(todoArray[_todoIndex])).add(content);
+}
+
+function tFGet(uint256 _todoIndex, uint256 _whichTodo) public view returns (string memory){
+return Todo(address(todoArray[_todoIndex])).get(_whichTodo).content;
+}
+
+
+}
